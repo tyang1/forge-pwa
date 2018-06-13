2e751ecd2c9a1f6a3eb1cd6092490d54765e67f7
Christopher <chris132128@gmail.com>
2018-06-13 14:21:38 -0700

    react ready, server running

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app-container">
+        <p>hello</p> World
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./client/index.js"
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: "css-loader" },
+      { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx$/, use: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
+
+module.exports = config;
