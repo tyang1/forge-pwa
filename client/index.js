c61105b78df25b42027e8c8d0de7ff889fc9ebfd
Kyle <kaloftin@hotmail.com>
2018-06-14 09:40:32 -0700

    crappy-navbar

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
 
-import App from "./App.jsx";
+import App from './Components/App.jsx';
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
