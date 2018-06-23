aca4ecc5a99373ba4d62daec3232881facf0af2d
Jeff <jeffreyrkang@gmail.com>
2018-06-23 11:18:13 -0700

    Change icons

--- client/Components/Features.jsx
+++ client/Components/Features.jsx
@@ -10,14 +10,14 @@ const Features = () => (
         <p>TODO: ADD GIF</p>
       </div>
       <div className="col-md-6">
-        <i className="fa fa-check fa-2x circled bg-skin float-left" />
-        <h4>Generate PWA Codebase</h4>
+        <i className="fas fa-bolt fa-2x circled bg-skin float-left" />
+        <h4> Generate PWA Codebase</h4>
         <p>Quickly generate a new directory that contains all the code you need to get up and running with a Progressive Web App.</p>
-        <i className="fa fa-check fa-2x circled bg-skin float-left" />
-        <h4>Host to a Firebase Account</h4>
+        <i className="fas fa-bolt fa-2x circled bg-skin float-left" />
+        <h4> Host to a Firebase Account</h4>
         <p>Deploy your forged Progressive Web App with a Google Firebase account.</p>
-        <i className="fa fa-check fa-2x circled bg-skin float-left" />
-        <h4>Host to your AWS Account</h4>
+        <i className="fas fa-bolt fa-2x circled bg-skin float-left" />
+        <h4> Host to your AWS Account</h4>
         <p>Automatically spins up an AWS EC2 instance and hosts your forged project in a S3 bucket.</p>
       </div>
     </div>
