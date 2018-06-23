2b935cfc425497f079ed1d02ccd7d4eda3418222
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-06-23 11:19:16 -0700

    adding github hook to picture

--- client/Components/Info.jsx
+++ client/Components/Info.jsx
@@ -4,7 +4,7 @@ const Info = props => (
   <div className = "team-hover">
     <div className="head">
       <h2>{props.bkground}</h2>
-      <a href={props.github}> <img border="0" alt="W3Schools" src="logo_w3s.gif" width="100" height="100"/>
+      <a href={props.github}> <img border="0" src={props.github} width="50" height="50"/>
       </a> 
     </div>
   </div>
