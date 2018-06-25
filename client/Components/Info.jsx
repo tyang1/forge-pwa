e6ea3613bfb6c59448ed3a9d7d0048d0b8b57677
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-06-25 14:21:30 -0700

    adjust the alignment for team

--- client/Components/Info.jsx
+++ client/Components/Info.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Info = props => (
   <div className = "team-hover">
     <div className="head">
+      <p>{props.description}</p>
       <a href={props.github}> <i className="fab fa-github fa-2x" />
       </a>
       <a href={props.linked}><i class="fab fa-linkedin fa-2x"></i></a> 
