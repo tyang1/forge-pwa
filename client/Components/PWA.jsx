4f8c765c220429aa0a8bdeedd0daf5d7dded5b66
Christopher <chris132128@gmail.com>
2018-06-24 23:24:04 -0700

    centered team images, updated them for mobile

--- client/Components/PWA.jsx
+++ client/Components/PWA.jsx
@@ -3,11 +3,20 @@ import Info from "./Info.jsx";
 
 const PWA = props => (
   <div className="info">
-    <img className="image" src={props.img} height={200} width={170} alt="" />
+    <img
+      className="image"
+      src={props.img}
+      // height={200}
+      // width={170}
+      alt={props.name}
+    />
     <p font-size="40px"> {props.name}</p>
     <p>{props.description}</p>
-    <Info bkground={props.bkground} github={props.github} linked={props.linked}/>
-    
+    <Info
+      bkground={props.bkground}
+      github={props.github}
+      linked={props.linked}
+    />
   </div>
 );
 
--- client/Components/Team.jsx
+++ client/Components/Team.jsx
@@ -4,50 +4,57 @@ import chris from "../static/Chris.jpg";
 import jeff from "../static/Jeff.jpg";
 import kyle from "../static/Kyle.jpg";
 import tiff from "../static/Tiffany.jpg";
-import Info from './Info.jsx';
 import "../animate.css/animate.min.css";
-import ScrollAnimation from 'react-animate-on-scroll';
-
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Team = props => (
   <div className="container-fluid team">
-<ScrollAnimation animateIn="fadeInRight">
-    <div className="row heading">
-      <h3>Our Team</h3>
-    </div>
-    <div className="column">
-      <PWA img={chris} name="Christopher Washburn" description='-"Passionate about React/Redux"' bkground="background" linked = "https://www.linkedin.com/in/christopherwashburn/" github="https://github.com/SKChristopher"/>
-    </div>
-    <div className="column">
-      <PWA img={kyle} name="Kyle Loftin" description='-"Love all things tech!"' bkground="ready to take on new tech" linked = "https://www.linkedin.com/in/kyle-loftin-62566639/" github="https://github.com/KALoftin"/>
-    </div>
-    <div className="column">
-      <PWA img={tiff} name="Tiffany Yang" description='-"Have fun with problem solving"' bkground="background" linked = "https://www.linkedin.com/in/tiffany-yang-09b82937/" github="https://github.com/tyang1"/>
-    </div>
-    <div className="column">
-      <PWA img={jeff} name="Jeffrey Kang" description='-"Enjoys fullstack!"' bkground="background" linked = "https://www.linkedin.com/in/jeff-r-kang/" github="https://github.com/jkang215"/>
-    </div>
-  </ScrollAnimation>
-    {/* <div className="column">
-      <PWA img={jeff} description="Jeff Kang" />
-    <div className='team-hover'>
-      <Info />
+    <ScrollAnimation animateIn="fadeInRight">
+      <div className="heading">
+        <h3>Our Team</h3>
       </div>
-    </div>
-
-    <div className="column">
-      <PWA img={kyle} description="Kyle" />
-    <div className='team-hover'>
-      <Info />
+      <div className="column">
+        <PWA
+          img={chris}
+          name="Christopher Washburn"
+          description="-&quot;Passionate about React/Redux&quot;"
+          bkground="background"
+          linked="https://www.linkedin.com/in/christopherwashburn/"
+          github="https://github.com/SKChristopher"
+        />
       </div>
-    </div>
-
-    <div className="column">
-      <PWA img={tiff} description="Tiffany Yang" />
-    <div className='team-hover'>
-      <Info />
+      <div className="column">
+        <PWA
+          img={kyle}
+          name="Kyle Loftin"
+          description="-&quot;Love all things tech!&quot;"
+          bkground="ready to take on new tech"
+          linked="https://www.linkedin.com/in/kyle-loftin-62566639/"
+          github="https://github.com/KALoftin"
+        />
       </div>
-    </div> */}
+      <div className="column">
+        <PWA
+          img={tiff}
+          name="Tiffany Yang"
+          description="-&quot;Have fun with problem solving&quot;"
+          bkground="background"
+          linked="https://www.linkedin.com/in/tiffany-yang-09b82937/"
+          github="https://github.com/tyang1"
+        />
+      </div>
+      <div className="column">
+        <PWA
+          img={jeff}
+          name="Jeffrey Kang"
+          description="-&quot;Enjoys fullstack!&quot;"
+          bkground="background"
+          linked="https://www.linkedin.com/in/jeff-r-kang/"
+          github="https://github.com/jkang215"
+        />
+      </div>
+    </ScrollAnimation>
   </div>
 );
+
 export default Team;
