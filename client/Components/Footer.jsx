3d1327f5d4c28c5906b6aadfeb9b25acaa5dec70
Christopher <chris132128@gmail.com>
2018-06-26 09:46:30 -0700

    updated footer links

--- client/Components/Footer.jsx
+++ client/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => (
   <nav className="navbar fixed-bottom navbar-dark bg-dark footer">
-    <a className="navbar-brand" href="https://github.com/ProgrammersWitAttitudes/pwa_creator">The Forge</a>
+    <a className="navbar-brand" href="https://github.com/forgepwa/the_forge">The Forge</a>
     <span className="navbar-text f-text"><em>This website was deployed with the Forge.</em></span>
-    <a href="https://github.com/ProgrammersWitAttitudes/pwa_creator/issues" target="_blank" rel="noopener noreferrer">Submit Feedback</a>
+    <a href="https://github.com/forgepwa/the_forge/issues" target="_blank" rel="noopener noreferrer">Submit Feedback</a>
   </nav>
 );
 
